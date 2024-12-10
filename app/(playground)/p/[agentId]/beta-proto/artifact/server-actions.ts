398a5d6b244889bbe80b3024f7fc49bd8ccf9d1d
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-12-10 12:33:46 +0900

    Remove variables unrelated to business logic

--- app/(playground)/p/[agentId]/beta-proto/artifact/server-actions.ts
+++ app/(playground)/p/[agentId]/beta-proto/artifact/server-actions.ts
@@ -3,9 +3,8 @@
 import { streamObject } from "ai";
 import { createStreamableValue } from "ai/rsc";
 
-import { getCurrentMeasurementScope, isRoute06User } from "@/app/(auth)/lib";
 import { langfuseModel } from "@/lib/llm";
-import { createLogger } from "@/lib/opentelemetry";
+import { createLogger, withTokenMeasurement } from "@/lib/opentelemetry";
 import { waitUntil } from "@vercel/functions";
 import { Langfuse } from "langfuse";
 import { schema as artifactSchema } from "../artifact/schema";
@@ -71,36 +70,25 @@ ${sourcesToText(sources)}
 				prompt: params.userPrompt,
 				schema: artifactSchema,
 				onFinish: async (result) => {
-					const duration = performance.now() - startTime;
-					const measurementScope = await getCurrentMeasurementScope();
-					const isR06User = await isRoute06User();
-					generation.end({
-						output: result,
-					});
-
-					logger.info(
-						{
-							externalServiceName: "openai",
-							tokenConsumedInput: result.usage.promptTokens,
-							tokenConsumedOutput: result.usage.completionTokens,
-							duration,
-							measurementScope,
-							isR06User,
-						},
-						"response obtained",
-					);
-
-					await lf.shutdownAsync();
-					waitUntil(
-						new Promise((resolve) =>
-							setTimeout(
-								resolve,
-								Number.parseInt(
-									process.env.OTEL_EXPORT_INTERVAL_MILLIS ?? "1000",
+					await withTokenMeasurement(
+						logger,
+						async () => {
+							generation.end({ output: result });
+							await lf.shutdownAsync();
+							waitUntil(
+								new Promise((resolve) =>
+									setTimeout(
+										resolve,
+										Number.parseInt(
+											process.env.OTEL_EXPORT_INTERVAL_MILLIS ?? "1000",
+										),
+									),
 								),
-							),
-						),
-					); // wait until telemetry sent
+							); // wait until telemetry sent
+							return result;
+						},
+						startTime,
+					);
 				},
 			});
 
